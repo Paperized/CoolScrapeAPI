e98adffc7e596320040267591b36a989563799f4
Paperized <ivanlogreco98@outlook.it>
2023-11-28 23:13:34 +0100

    - Able to scrape various amazon products with different type of offers

--- src/main/java/com/paperized/shopapi/scraper/impl/AmazonScraperImpl.java
+++ src/main/java/com/paperized/shopapi/scraper/impl/AmazonScraperImpl.java
@@ -6,12 +6,12 @@ import com.paperized.shopapi.dto.WebsiteSetting;
 import com.paperized.shopapi.scraper.AmazonScraper;
 import com.paperized.shopapi.scraper.ScraperHttpService;
 import com.paperized.shopapi.utils.ScraperUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.HttpStatusException;
 import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class AmazonScraperImpl implements AmazonScraper {
@@ -32,14 +32,32 @@ public class AmazonScraperImpl implements AmazonScraper {
 
         amazonProduct.setName(ScraperUtils.getText(page.getElementById("productTitle")));
 
+        if (extractDetailsRedOfferPanel(page, amazonProduct))
+            return amazonProduct;
+
+        // this is used for both discounted products and normal products since they have both a currentPrice common field
+        extractDetailsOfferPanel(page, amazonProduct);
+        return amazonProduct;
+    }
+
+    private boolean extractDetailsRedOfferPanel(Document page, AmazonProduct amazonProduct) {
         var pricePanel = page.selectXpath("//*[starts-with(@id, 'corePrice_desktop')]").first();
-        if(pricePanel != null) {
+        if (pricePanel != null) {
             var lastPrice = ScraperUtils.getText(pricePanel.selectXpath(".//*[@data-a-strike='true']/*[1]").first());
             var currentPrice = ScraperUtils.getText(pricePanel.selectXpath(".//*[@id='taxInclusiveMessage']/../*[@data-a-color='price']/*[1]").first());
             var savingPrice = ScraperUtils.getText(pricePanel.selectXpath("//*[contains(@id, 'bundleLTBSSavings')]/..//*[@data-a-color='price']/*[1]").first());
             amazonProduct.lastPrice(lastPrice).currentPrice(currentPrice).savingPrice(savingPrice);
         }
 
-        return amazonProduct;
+        return StringUtils.isNoneEmpty(amazonProduct.getLastPrice(), amazonProduct.getCurrentPrice(), amazonProduct.getSavingPrice());
+    }
+
+    private void extractDetailsOfferPanel(Document page, AmazonProduct amazonProduct) {
+        var pricePanel = page.selectXpath("//*[starts-with(@id, 'corePriceDisplay')]").first();
+        if (pricePanel != null) {
+            var currentPrice = ScraperUtils.getText(pricePanel.selectXpath(".//*[@id='taxInclusiveMessage']/../span[contains(@class, 'priceToPay')]").first());
+            var suggestedPrice = ScraperUtils.getText(pricePanel.selectXpath(".//span[contains(@class, 'basisPrice')]//span[@data-a-strike]/*[1]").first());
+            amazonProduct.currentPrice(currentPrice).suggestedPrice(suggestedPrice);
+        }
     }
 }
