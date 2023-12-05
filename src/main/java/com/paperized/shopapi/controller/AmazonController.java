1d34fce892018090683598d01b5511f15e7e5930
Paperized <ivanlogreco98@outlook.it>
2023-12-05 02:05:32 +0100

    - Code generation with mustache for Queryable models

--- src/main/java/com/paperized/shopapi/controller/AmazonController.java
+++ src/main/java/com/paperized/shopapi/controller/AmazonController.java
@@ -4,6 +4,7 @@ import com.paperized.generated.shopapi.api.AmazonApi;
 import com.paperized.generated.shopapi.model.AmazonProductTracked;
 import com.paperized.shopapi.dquery.QueryCondition;
 import com.paperized.shopapi.service.AmazonService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,12 @@ public class AmazonController implements AmazonApi {
 
     @Override
     public ResponseEntity<AmazonProductTracked> findProductDetails(String url, QueryCondition body) throws Exception {
-        return ResponseEntity.ok(amazonService.findProductDetails(url));
+        AmazonProductTracked amazonProductTracked = amazonService.findProductDetails(url);
+
+        // just for testing, works!!
+        if(!body.evaluate(amazonProductTracked.getItem()))
+            throw new EntityNotFoundException("Entity did not pass the query check!");
+
+        return ResponseEntity.ok(amazonProductTracked);
     }
 }
--- src/main/java/com/paperized/shopapi/model/RegisteredProductTracking.java
+++ src/main/java/com/paperized/shopapi/model/RegisteredProductTracking.java
@@ -1,13 +1,12 @@
 package com.paperized.shopapi.model;
 
-import com.paperized.shopapi.dquery.DQueriable;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Table(name = "REGISTERED_PRODUCT_TRACKING")
 @Data
-public class RegisteredProductTracking implements DQueriable {
+public class RegisteredProductTracking {
     @Id
     @Column(name = "ID")
     private String id;
@@ -20,15 +19,4 @@ public class RegisteredProductTracking implements DQueriable {
 
     @OneToOne(mappedBy = "registeredProductTracking")
     private ProductTracking productTracking;
-
-    @Override
-    public Object getVariableValue(String name) {
-        return switch (name) {
-            case "id" -> id;
-            case "webhookUrl" -> webhookUrl;
-            case "intervalDuration" -> intervalDuration;
-            case "productTracking" -> productTracking;
-            default -> throw new RuntimeException(name + " is not a properties of " + getClass().getSimpleName());
-        };
-    }
 }
