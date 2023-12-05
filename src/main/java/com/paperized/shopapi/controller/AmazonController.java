e173b62935916806fb6e7e57f7080d239397de0a
Paperized <ivanlogreco98@outlook.it>
2023-12-05 02:23:46 +0100

    - TODO for me of the future

--- src/main/java/com/paperized/shopapi/controller/AmazonController.java
+++ src/main/java/com/paperized/shopapi/controller/AmazonController.java
@@ -20,6 +20,9 @@ public class AmazonController implements AmazonApi {
     public ResponseEntity<AmazonProductTracked> findProductDetails(String url, QueryCondition body) throws Exception {
         AmazonProductTracked amazonProductTracked = amazonService.findProductDetails(url);
 
+        // TODO: [HIGH] add evaluation on two entities capabilities, add core binary operations
+        // TODO: [LOW+] add order by capabilities
+        // TODO: [LOW] one day add validation check (prob via runtime annotation), integrate QueryCondition with JPA to query directly on the database (No fancy queries just conditions, no grouping, subquery etc...)
         // just for testing, works!!
         if(!body.evaluate(amazonProductTracked.getItem()))
             throw new EntityNotFoundException("Entity did not pass the query check!");
